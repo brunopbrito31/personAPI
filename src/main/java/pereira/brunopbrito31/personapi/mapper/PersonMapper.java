4644618647cfbfb6ff49a65f76f4951ac4ebb0af
Bruno Brito <brunopbrito31@hotmail.com>
2021-07-12 04:50:35 -0300

    second part of project

--- src/main/java/pereira/brunopbrito31/personapi/mapper/PersonMapper.java
+++ src/main/java/pereira/brunopbrito31/personapi/mapper/PersonMapper.java
@@ -11,6 +11,8 @@ public interface PersonMapper {
 
     PersonMapper INSTANCE = Mappers.getMapper(PersonMapper.class);
 
+
+    //preciso resolver esse mapeamento
     @Mapping(target = "birthDate", source = "birthDate", dateFormat = "dd-MM-yyyy")
     Person toModel(PersonDTO personDTO);
 
